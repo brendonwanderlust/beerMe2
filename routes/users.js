0bc7ab6f90bf442670ad86c5635ac314898fb80b
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-06-09 13:52:29 -0400

    Lachlan did a bunch of shit

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    bodyclass: 'login'
+  });
 });
 
 module.exports = router;
