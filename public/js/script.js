3f707539633643a5955a70d5900051c31589681a
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-06-09 17:10:16 -0400

    refactored the script.js file a bit

--- public/js/script.js
+++ public/js/script.js
@@ -5,29 +5,25 @@ $(function() {
     let breweryList = [];
     let beerList = [];
 
-
-    api.searchForBreweries()
-        .then(function(breweries) {
-            breweryList = breweries;
-            //Render Map here
-            //console.log(map);
-            let mapped = new Map(breweries);            
-            mapped.render(breweries);
-            breweries.forEach(function(brewery) {
-                api.getBeersForBrewery(brewery.name)
-                    .then(function(beers) {
-                        beers.forEach(function(beer) {
-                            beerList.push(beer);
-                            sidebar.renderSideBar(beerList);
-                        });
-                    })
-                    .catch(function(error) {
-                        console.log(error);
-                    });
+    api.searchForBreweries()    
+    .then( function(breweries) {
+        let mapped = new Map(breweries);            
+        mapped.render(breweries);
+        return breweries
+    }).then( function(breweries) {
+        breweries.forEach(function (brewery) {
+            api.getBeersForBrewery(brewery.name)
+            .then(function (beers) {
+                beers.forEach(function (beer) {
+                    beerList.push(beer);
+                    sidebar.renderSideBar(beerList);
+                });
+            })
+            .catch(function (error) {
+                console.log("broken inside second promise. Error: " + error);
             });
-        })
-        .catch(function(error) {
-            console.log(error);
         });
-    
-});
+    }).catch(function(error) {
+        console.log(error);
+    });
+})
