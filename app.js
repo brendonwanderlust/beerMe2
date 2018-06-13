d34dcbb103bf4aaede0a113f6ffb366f2d5903e9
Joseph Scott <jbs198585@gmail.com>
2018-06-12 20:45:10 -0400

    Made all the routes work and added Favorites

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const User = require('./models/user');
 const FavoriteBeers = require('./models/favoriteBeer');
 const FavoriteBreweries = require('./models/favoriteBrewery');
+var hbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,7 @@ var appRouter = require('./routes/app');
 var app = express();
 
 // view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -29,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use('/app', appRouter);
 
 // catch 404 and forward to error handler
