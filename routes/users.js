f4ffbeb8a0c206e6e75220bc660cb509220fa4d5
Gabriel Edouard <edgabriel92@gmail.com>
2018-06-14 15:13:11 -0400

    added login authentication to users.js, and a few changes to app.hbs and login.hbs for testing

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const ensureAuthenticated = require('../auth').ensureAuthenticated();
+const User = require('../models/user');
+
+router.all('*', ensureAuthenticated);
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-  res.render('login', {
-    bodyclass: 'login'
+    User.findById(req.user)
+    .then(user => {
+      res.render('login', {
+        bodyclass: 'login',
+        isLoggedIn: req.isAuthenticated(),
+        user: User.findById(1)  
+    })
   });
 });
 
