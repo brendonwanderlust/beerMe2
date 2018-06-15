71a367d663ad7992ea1e8add3513710a32f4fabb
Joseph Scott <jbs198585@gmail.com>
2018-06-14 21:20:11 -0400

    added beer icon

--- public/js/beers.js
+++ public/js/beers.js
@@ -9,14 +9,10 @@ class Beers {
         
         beerHTML += '<div class="beer card">';
         beerHTML += '<div class="card-body">';
-        beerHTML += '<div>'
         beerHTML += '<h4 class="card-title td">'+ this.beer.beer_name +'</h4>';
         beerHTML += ' <i class="fas fa-beer"></i>';
-        beerHTML += '</div>';
-        beerHTML += '<div>';
         beerHTML += '<h5 class="card-subtitle td">'+ this.beer.brewery_name +'</h5>';
         beerHTML += ' <i class="fas fa-beer"></i>';
-        beerHTML += '</div>';
         beerHTML += '<a href="https://www.beeradvocate.com' + this.beer.beer_url + '" target="_blank">View on BeerAdvocate</a>';
         beerHTML += '</div>';
         beerHTML += '</div>';
